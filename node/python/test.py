ae7a1ff956b3e0c34d4147c5f684ead5e429a27b
Erik T <etrygg@gmail.com>
2020-02-09 22:05:48 +0100

    Minor fix.

--- node/python/test.py
+++ node/python/test.py
@@ -3,7 +3,8 @@ from signals import Signal
 import threading, time
 
 if __name__ == "__main__":
-    node = node.Node('192.168.10.183', 80)
+    #node = node.Node('192.168.10.183', 80)
+    node = node.Node('127.0.0.1', 10030)
 
     sign = Signal(0, 1, Signal.SC_OUT, Signal.ST_BOOL, "Flip", value = False)
     node.AddSignal(sign)
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     lock = threading.Lock()
     msg = []
     def _async_update_loop_():
+        time.sleep(0.1)
         node.start()
         while True: # Loop and update values each cycle
             lock.acquire()
@@ -27,7 +29,8 @@ if __name__ == "__main__":
     thread = threading.Thread(target= _async_update_loop_)
     thread.start()
 
-    input("[Press Enter to exit]")
+    print("[Press Enter to exit]")
+    input()
 
     lock.acquire()
     msg.append('stop')
