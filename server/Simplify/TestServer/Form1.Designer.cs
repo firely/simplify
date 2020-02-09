1d2d0c7a724d1cc455469ebbc3c473921b15981b
Erik T <etrygg@gmail.com>
2020-02-09 22:11:14 +0100

    Created Visual Studio project for server functionality.

---
+++ server/Simplify/TestServer/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TestServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(12, 12);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.Start);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(12, 41);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(75, 23);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.Stop);
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Enabled = true;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateHMI);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(193, 12);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(263, 368);
+            this.ListBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 437);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Timer UpdateTimer;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ server/Simplify/TestServer/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Simplify;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestServer
+{
+    public partial class Form1 : Form
+    {
+        NetworkSocket socket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            if (socket != null)
+                return;
+            socket = new NetworkSocket();
+            socket.Start();
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+            if (socket == null)
+                return;
+            socket.Stop();
+            socket = null;
+        }
+
+        private void UpdateHMI(object sender, EventArgs e)
+        {
+            if (socket == null)
+                return;
+
+            var buffer = socket.GetReadData();
+            foreach (var item in buffer)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var c in item)
+                {
+                    sb.Append(String.Format("{0,2:X} ", c));
+                }
+                ListBox.Items.Add(sb.ToString());
+            }
+        }
+    }
+}
